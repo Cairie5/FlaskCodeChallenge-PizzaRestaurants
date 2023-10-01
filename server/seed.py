2ffb604faf982effc4e76eddb7d963097155e746
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-10-01 21:11:22 +0300

    final

--- server/seed.py
+++ server/seed.py
@@ -1,35 +1,34 @@
 # seed.py
 
-from app import db, Restaurant, Pizza, RestaurantPizza
+from app import db, Restaurant, Pizza, RestaurantPizza, app
 
 def seed_data():
-    # Create some restaurants
-    restaurant1 = Restaurant(name="Dominion Pizza", address="Good Italian, Ngong Road, 5th Avenue")
-    restaurant2 = Restaurant(name="Pizza Hut", address="Westgate Mall, Mwanzi Road, Nrb 100")
+    with app.app_context():
+        # Create some restaurants
+        restaurant1 = Restaurant(name="Dominion Pizza", address="Good Italian, Ngong Road, 5th Avenue")
+        restaurant2 = Restaurant(name="Pizza Hut", address="Westgate Mall, Mwanzi Road, Nrb 100")
 
-    # Create some pizzas
-    pizza1 = Pizza(name="Cheese", ingredients="Dough, Tomato Sauce, Cheese")
-    pizza2 = Pizza(name="Pepperoni", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
+        # Create some pizzas
+        pizza1 = Pizza(name="Cheese", ingredients="Dough, Tomato Sauce, Cheese")
+        pizza2 = Pizza(name="Pepperoni", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
 
-    # Add pizzas to restaurants
-    restaurant1.pizzas.append(pizza1)
-    restaurant1.pizzas.append(pizza2)
-    restaurant2.pizzas.append(pizza1)
+        # Add data to the session and commit
+        db.session.add(restaurant1)
+        db.session.add(restaurant2)
+        db.session.add(pizza1)
+        db.session.add(pizza2)
+        db.session.commit()
 
-    # Create some restaurant-pizza associations
-    restaurant_pizza1 = RestaurantPizza(price=10, pizza=pizza1, restaurant=restaurant1)
-    restaurant_pizza2 = RestaurantPizza(price=12, pizza=pizza2, restaurant=restaurant1)
-    restaurant_pizza3 = RestaurantPizza(price=11, pizza=pizza1, restaurant=restaurant2)
+        # Create some restaurant-pizza associations
+        restaurant_pizza1 = RestaurantPizza(price=10, pizza_id=pizza1.id, restaurant_id=restaurant1.id)
+        restaurant_pizza2 = RestaurantPizza(price=12, pizza_id=pizza2.id, restaurant_id=restaurant1.id)
+        restaurant_pizza3 = RestaurantPizza(price=11, pizza_id=pizza1.id, restaurant_id=restaurant2.id)
 
-    # Add data to the session and commit
-    db.session.add(restaurant1)
-    db.session.add(restaurant2)
-    db.session.add(pizza1)
-    db.session.add(pizza2)
-    db.session.add(restaurant_pizza1)
-    db.session.add(restaurant_pizza2)
-    db.session.add(restaurant_pizza3)
-    db.session.commit()
+        # Add the restaurant-pizza associations to the session and commit
+        db.session.add(restaurant_pizza1)
+        db.session.add(restaurant_pizza2)
+        db.session.add(restaurant_pizza3)
+        db.session.commit()
 
 if __name__ == '__main__':
     seed_data()
